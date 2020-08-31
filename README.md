badb68703ec32757267df77b1719b30c74d32fd3
Adam Múdry <mudry.ado@gmail.com>
2020-08-31 04:36:21 +0200

    init

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
-# vut-fit-ial
-IAL - Algoritmy
+# IAL - Algoritmy
+
+```c202.c``` - zásobník znakov v poli
+
+```c204.c``` - prevod infixového výrazu na postfixový
+
+```c206.c``` - dvojsmerne viazaný lineárny zoznam
+
+```c401.c``` - binárny strom a operácie nad ním implementované rekurzívne
+
+```c402.c``` - binárny strom a operácie nad ním implementované nerekurzívne
+
+```c016.c``` - tabulka s rozptýlenými položkami
---
+++ c204.c
@@ -0,0 +1,204 @@
+
+/* ******************************* c204.c *********************************** */
+/*  Předmět: Algoritmy (IAL) - FIT VUT v Brně                                 */
+/*  Úkol: c204 - Převod infixového výrazu na postfixový (s využitím c202)     */
+/*  Referenční implementace: Petr Přikryl, listopad 1994                      */
+/*  Přepis do jazyka C: Lukáš Maršík, prosinec 2012                           */
+/*  Upravil: Kamil Jeřábek, září 2019                                         */
+/* ************************************************************************** */
+/*
+** Implementujte proceduru pro převod infixového zápisu matematického
+** výrazu do postfixového tvaru. Pro převod využijte zásobník (tStack),
+** který byl implementován v rámci příkladu c202. Bez správného vyřešení
+** příkladu c202 se o řešení tohoto příkladu nepokoušejte.
+**
+** Implementujte následující funkci:
+**
+**    infix2postfix .... konverzní funkce pro převod infixového výrazu 
+**                       na postfixový
+**
+** Pro lepší přehlednost kódu implementujte následující pomocné funkce:
+**    
+**    untilLeftPar .... vyprázdnění zásobníku až po levou závorku
+**    doOperation .... zpracování operátoru konvertovaného výrazu
+**
+** Své řešení účelně komentujte.
+**
+** Terminologická poznámka: Jazyk C nepoužívá pojem procedura.
+** Proto zde používáme pojem funkce i pro operace, které by byly
+** v algoritmickém jazyce Pascalovského typu implemenovány jako
+** procedury (v jazyce C procedurám odpovídají funkce vracející typ void).
+**
+**/
+
+#include "c204.h"
+#include <stdio.h> //////
+////
+int solved;
+
+/*
+** Pomocná funkce untilLeftPar.
+** Slouží k vyprázdnění zásobníku až po levou závorku, přičemž levá závorka
+** bude také odstraněna. Pokud je zásobník prázdný, provádění funkce se ukončí.
+**
+** Operátory odstraňované ze zásobníku postupně vkládejte do výstupního pole
+** znaků postExpr. Délka převedeného výrazu a též ukazatel na první volné
+** místo, na které se má zapisovat, představuje parametr postLen.
+**
+** Aby se minimalizoval počet přístupů ke struktuře zásobníku, můžete zde
+** nadeklarovat a používat pomocnou proměnnou typu char.
+*/
+void untilLeftPar ( tStack* s, char* postExpr, unsigned* postLen ) {
+  
+  char tmp;
+  
+  while (!stackEmpty(s)) { // pokým nie je stack prázdny
+    stackTop(s, &tmp); //zistím čo je na vrchu
+    stackPop(s); // odstránim to
+    if (tmp == '(') break;
+    postExpr[(*postLen)++] = tmp; // prekopírujem do výslednej premennej
+  }
+}
+
+/*
+** Pomocná funkce doOperation.
+** Zpracuje operátor, který je předán parametrem c po načtení znaku ze
+** vstupního pole znaků.
+**
+** Dle priority předaného operátoru a případně priority operátoru na
+** vrcholu zásobníku rozhodneme o dalším postupu. Délka převedeného 
+** výrazu a taktéž ukazatel na první volné místo, do kterého se má zapisovat, 
+** představuje parametr postLen, výstupním polem znaků je opět postExpr.
+*/
+void doOperation ( tStack* s, char c, char* postExpr, unsigned* postLen ) {
+
+  if (stackEmpty(s)) {
+    stackPush(s, c);
+    return;
+
+  } else {
+    char ch;
+    stackTop(s, &ch);
+    
+    switch (c) {
+      case '+':
+      case '-':
+
+        if (ch == '('){
+          stackPush(s, c);  
+          return;
+        }
+        break;
+      
+      case '*':
+      case '/':
+          
+        if (ch == '(' || ch == '+' || ch == '-') {
+          stackPush(s, c);  
+          return;
+        }
+        break;
+    }
+
+    postExpr[(*postLen)++] = ch;
+    stackPop(s);
+    doOperation(s, c, postExpr, postLen);
+  }
+}
+
+/* 
+** Konverzní funkce infix2postfix.
+** Čte infixový výraz ze vstupního řetězce infExpr a generuje
+** odpovídající postfixový výraz do výstupního řetězce (postup převodu
+** hledejte v přednáškách nebo ve studijní opoře). Paměť pro výstupní řetězec
+** (o velikosti MAX_LEN) je třeba alokovat. Volající funkce, tedy
+** příjemce konvertovaného řetězce, zajistí korektní uvolnění zde alokované
+** paměti.
+**
+** Tvar výrazu:
+** 1. Výraz obsahuje operátory + - * / ve významu sčítání, odčítání,
+**    násobení a dělení. Sčítání má stejnou prioritu jako odčítání,
+**    násobení má stejnou prioritu jako dělení. Priorita násobení je
+**    větší než priorita sčítání. Všechny operátory jsou binární
+**    (neuvažujte unární mínus).
+**
+** 2. Hodnoty ve výrazu jsou reprezentovány jednoznakovými identifikátory
+**    a číslicemi - 0..9, a..z, A..Z (velikost písmen se rozlišuje).
+**
+** 3. Ve výrazu může být použit předem neurčený počet dvojic kulatých
+**    závorek. Uvažujte, že vstupní výraz je zapsán správně (neošetřujte
+**    chybné zadání výrazu).
+**
+** 4. Každý korektně zapsaný výraz (infixový i postfixový) musí být uzavřen 
+**    ukončovacím znakem '='.
+**
+** 5. Při stejné prioritě operátorů se výraz vyhodnocuje zleva doprava.
+**
+** Poznámky k implementaci
+** -----------------------
+** Jako zásobník použijte zásobník znaků tStack implementovaný v příkladu c202. 
+** Pro práci se zásobníkem pak používejte výhradně operace z jeho rozhraní.
+**
+** Při implementaci využijte pomocné funkce untilLeftPar a doOperation.
+**
+** Řetězcem (infixového a postfixového výrazu) je zde myšleno pole znaků typu
+** char, jenž je korektně ukončeno nulovým znakem dle zvyklostí jazyka C.
+**
+** Na vstupu očekávejte pouze korektně zapsané a ukončené výrazy. Jejich délka
+** nepřesáhne MAX_LEN-1 (MAX_LEN i s nulovým znakem) a tedy i výsledný výraz
+** by se měl vejít do alokovaného pole. Po alokaci dynamické paměti si vždycky
+** ověřte, že se alokace skutečně zdrařila. V případě chyby alokace vraťte namísto
+** řetězce konstantu NULL.
+*/
+void (*func)(void (*[])(int,char));
+
+char* infix2postfix (const char* infExpr) {
+
+  // inicializácia premennej a alokácia pamäte pre výsledný "string"
+  char *postExpr = (char*) malloc(sizeof(char)*MAX_LEN);
+  if (!postExpr) return NULL;
+  // inicialiazácia  premennej pre dĺžku postExpr
+  unsigned postLen = 0;
+
+  // inicializácia stacku
+  tStack stack;
+  stackInit(&stack); 
+
+  while (*infExpr != '=') { // pokým sa vo vstupnom stringu neobjaví "="
+    switch (*infExpr) {
+   
+      case '0': case '1': case '2': case '3': case '4': case '5': case '6': case '7': case '8': case '9':
+      case 'a': case 'b': case 'c': case 'd': case 'e': case 'f': case 'g': case 'h': case 'i': case 'j': 
+        case 'k': case 'l': case 'm': case 'n': case 'o': case 'p': case 'q': case 'r': case 's': case 't': 
+        case 'u': case 'v': case 'w': case 'x': case 'y': case 'z':
+      case 'A': case 'B': case 'C': case 'D': case 'E': case 'F': case 'G': case 'H': case 'I': case 'J': 
+        case 'K': case 'L': case 'M': case 'N': case 'O': case 'P': case 'Q': case 'R': case 'S': case 'T': 
+        case 'U': case 'V': case 'W': case 'X': case 'Y': case 'Z':
+        
+        postExpr[postLen++] = *infExpr; // ak je charakter identifikátor, skopíruje sa do výslednej premennej
+        break;
+
+      case '+': case '-': case '*': case '/':
+        doOperation(&stack, *infExpr, postExpr, &postLen); 
+        break;
+      
+      case '(':
+        stackPush(&stack, *infExpr);
+        break;
+      
+      case ')':
+        untilLeftPar(&stack, postExpr, &postLen);
+        break;
+
+      default:
+        return NULL;
+    }
+    infExpr += 1;
+  }
+  untilLeftPar(&stack, postExpr, &postLen);
+  postExpr[postLen++] = '='; // pridá na koniec "="
+  postExpr[postLen++] = '\0'; // pridá null terminator
+  return postExpr;
+}
+
+/* Konec c204.c */
